be9c782fb00ec201f969e154c762f4e8f8ae88e5
match08 <match_2006@qq.com>
2020-09-10 10:29:41 +0800

    ios 这个模式是为了保证低包强更，上次版本app缓存还在的处理方式

--- src/ios/HCPPlugin.m
+++ src/ios/HCPPlugin.m
@@ -49,6 +49,24 @@ static NSString *const DEFAULT_STARTING_PAGE = @"index.html";
 #pragma mark Lifecycle
 
 -(void)pluginInitialize {
+    
+    //cap mode added build version
+    BOOL isApplicationUpdated = ![[NSBundle applicationBuildVersion] isEqualToString:_pluginInternalPrefs.appBuildVersion];
+    //如果build version 不等就重新热更流程(这个模式是为了保证低包强更，上次版本app缓存还在的处理方式)
+    if(isApplicationUpdated)
+    {
+       [self cleanupFileSystemFromOldReleases];
+       // clear preferences
+       [HCPPluginInternalPreferences clearFromUserDefaults];
+       // clear Capacitor mode webview baseServerPath
+       if ([NSStringFromClass([self.viewController class]) isEqualToString:@"Capacitor.CAPBridgeViewController"])
+       {
+          NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+          [userDefaults removeObjectForKey:@"serverBasePath"];
+          [userDefaults synchronize];
+       }
+    }
+    
     [self doLocalInit];
     [self subscribeToEvents];
     
